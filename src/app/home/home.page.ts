c3bb311b7bda6c8534269ae6f6491b1d3fa00740
nufke <nufke@barnasconi.net>
2021-12-27 23:31:00 +0100

    control implementation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IMqttMessage, MqttService } from 'ngx-mqtt';
 import { Subscription } from 'rxjs';
+import { util } from 'node-forge' // TODO check package
 
 // TODO move to interface object
-interface topic {
-  name: string;
-  message: string;
+interface control {
+  topic: string,
+  name: string,
+  icon: {
+    name: string,
+    href?: string,
+    color?: string
+  }
+  type: string,
+  room: string,
+  category: string,
+  is_favorite?: Boolean,
+  is_visible?: Boolean,
+  is_protected?: Boolean,
+  is_movable?: Boolean,
+  priority?: number,
+  state: {
+    value: string | Number,
+    format?: string,
+    color?: string,
+    message?: string // TODO move outside interface
+  }
 }
 
 @Component({
@@ -15,35 +35,109 @@ interface topic {
 })
 export class HomePage {
 
-  private subscription: Subscription;
-  public topics: topic[];
+  private subscription: Subscription[];
+
+  public controls: control[];
 
   constructor(private _mqttService: MqttService) {
-    this.topics = [ 
-      {name: 'test', message: 'waiting for value...'}, 
-      {name: 'test1', message: 'waiting for value...'} 
+    this.controls = [ 
+      {
+        topic: 'test/sensor1',
+        name: 'Sensor 1',
+        icon: {name: 'bulb'},
+        type: 'text',
+        room: 'Living Room',
+        category: 'Sensors',
+        priority: 1,
+        state: {
+          value: 'waiting for value...',
+          format: '%s'
+        }
+      }, 
+      {
+        topic: 'test/sensor2',
+        name: 'Sensor 2',
+        icon: {name: 'bulb'},
+        type: 'text',
+        room: 'Living Room',
+        category: 'Sensors',
+        priority: 2,
+        state: {
+          value: 'waiting for value...',
+          format: '%s °C'
+        }
+      }, 
     ]; 
-    
-    console.log('subscribe to topics');
-
-    this.topics.forEach( (topic) => 
-      this.subscription = this._mqttService.observe(topic.name).subscribe((message: IMqttMessage) => {
-        topic.message = message.payload.toString();
-        console.log(topic.name,':', topic.message);
-      })
-    );
+    this.subscription = [];
+    console.log('subscribe topics..');
+    this.registerControls();
   }
 
-  public unsafePublish(topic: string, message: string): void {
-    this._mqttService.unsafePublish(topic, message, {qos: 1, retain: true});
+  public registerControls(): void {
+
+    this.controls.forEach( (control) => {
+      this.subscription.push( this._mqttService.observe(control.topic).subscribe((message: IMqttMessage) => {
+        control.state.value = message.payload.toString();
+      }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/name').subscribe((message: IMqttMessage) => {
+        control.name = message.payload.toString();
+      }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/icon/name').subscribe((message: IMqttMessage) => {
+        control.icon.name = message.payload.toString();
+      }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/icon/color').subscribe((message: IMqttMessage) => {
+        control.icon.color = message.payload.toString();
+      }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/icon/href').subscribe((message: IMqttMessage) => {
+        control.icon.href = message.payload.toString();
+      }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/type').subscribe((message: IMqttMessage) => {
+        control.type = message.payload.toString();
+      }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/room').subscribe((message: IMqttMessage) => {
+        control.room = message.payload.toString();
+      }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/category').subscribe((message: IMqttMessage) => {
+        control.category = message.payload.toString();
+      }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/is_favorite').subscribe((message: IMqttMessage) => {
+        control.is_favorite = (message.payload.toString() === '1');
+      }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/is_visible').subscribe((message: IMqttMessage) => {
+        control.is_visible = (message.payload.toString() === '1');
+      }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/is_protected').subscribe((message: IMqttMessage) => {
+        control.is_protected = (message.payload.toString() === '1');
+      }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/priority').subscribe((message: IMqttMessage) => {
+        control.priority = Number(message.payload.toString());
+      }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/state/value').subscribe((message: IMqttMessage) => {
+        control.state.value = message.payload.toString();
+        control.state.message = message.payload.toString();
+        if (Number(control.state.value)) 
+          control.state.value = Number(control.state.value);
+        if (control.state.format.length)
+          control.state.message = util.format(control.state.format,control.state.value);
+        }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/state/format').subscribe((message: IMqttMessage) => {
+        control.state.format = message.payload.toString();
+        if (control.state.format.length)
+          control.state.message = util.format(control.state.format,control.state.value);
+      }));
+      this.subscription.push( this._mqttService.observe(control.topic+'/state/color').subscribe((message: IMqttMessage) => {
+        control.state.color = message.payload.toString();
+      }));
+    }); //forEach
   }
 
-  public onClick(){
-    console.log(this._mqttService);
+  public ngOnDestroy() : void {
+    console.log('unsubscribe topics..');
+    this.subscription.forEach( (item) => { item.unsubscribe(); } );
   }
-  
-  public ngOnDestroy() {
-    console.log('unsubscribe to topics');
-    this.subscription.unsubscribe();
+
+  public sortControls() : control[] {
+    return this.controls.sort( (a, b) => { return a.priority - b.priority });
   }
+
 }
