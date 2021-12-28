6652ef6405cd37cd78df511dd0540e2fc675b938
nufke <nufke@barnasconi.net>
2021-12-28 20:57:51 +0100

    room filter, add controls, add display grid

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,11 +10,17 @@ import { Control } from '../interfaces/control'
 export class HomePage {
   public controls: Control[];
 
+  public rooms: string[];
+
   constructor(public LoxBerryService: LoxBerry) {
     this.controls = [];
 
     this.LoxBerryService.load().subscribe((controls: Control[]) => {
       this.controls = controls;
+
+      this.rooms = this.controls.map(item => item.room)
+        .filter((value, index, self) => self.indexOf(value) === index)
+        .sort((a, b) => { return a.localeCompare(b); })
     });
   }
 
@@ -22,8 +28,50 @@ export class HomePage {
     this.LoxBerryService.unload();
   }
 
-  public sortControls() : Control[] {
-    return this.controls.sort( (a, b) => { return a.priority - b.priority });
+  public filterControls(room: any) : Control[] {
+    var filteredControls =  this.controls.filter( (resp) => { return resp.room == room });
+    return filteredControls.sort( (a, b) => { return a.priority - b.priority });
   }
 
+  pushed($event, control) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    console.log('pushed', control);
+  }
+
+  pushed_radio($event, control) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    console.log('pushed radio', control);
+  }
+
+  pushed_up($event, control) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    console.log('pushed up', control);
+  }
+  
+  pushed_down($event, control) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    console.log('pushed down', control);
+  }
+
+  pushed_plus($event, control) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    console.log('pushed plus', control);
+  }
+
+  pushed_minus($event, control) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    console.log('pushed minus', control);
+  }
+
+  toggle($event, control){
+    $event.preventDefault();
+    $event.stopPropagation();
+    console.log('toggle', control);
+  }
 }
